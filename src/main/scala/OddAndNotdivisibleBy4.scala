fe2e03de1ad60a88d1b602d3922c3903c0a623c3
ShruthiAnnamaneni <48015906+ShruthiAnnamaneni@users.noreply.github.com>
2025-02-21 23:11:57 +0530

    Update OddAndNotdivisibleBy4.scala

--- src/main/scala/OddAndNotdivisibleBy4.scala
+++ src/main/scala/OddAndNotdivisibleBy4.scala
@@ -4,9 +4,9 @@ def main(args: Array[String]): Unit={
 Check for Odd AND Not Divisible by 4:
 Create a Scala program to check if a given number is both odd and not divisible by 4.
 Sample Data:
-Number: 27
+Number: 15
    */
-  val num=27
+  val num=15
   if(num%2!=0 && num%4!=0){
     println(num+" is both odd and not divisible by 4")
   }
