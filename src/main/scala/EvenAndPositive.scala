3d63c7d9d7d4767df2532bed5b3480d0e823c4ca
Shruthi <ashruthi09@gmail.com>
2025-02-21 22:47:14 +0530

    Even And Positive

---
+++ src/main/scala/EvenAndPositive.scala
@@ -0,0 +1,9 @@
+object EvenAndPositive {
+def main(args: Array[String]): Unit={
+  //Check for Even AND Positive Number
+  val a= 14
+ if(a>0 & a%2==0){
+   println(a + " is even and positive")
+ }
+}
+}
